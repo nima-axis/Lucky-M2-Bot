56508e007f91124e209bac4f756815783eb4e028
NMD AXIS <nmdaxis@gmail.com>
2026-04-06 15:58:32 +0530

    Add files via upload

--- commands/ai/gptimage.js
+++ commands/ai/gptimage.js
@@ -5,7 +5,7 @@
 
 const axios = require('axios');
 const FormData = require('form-data');
-const { downloadMediaMessage } = require('@whiskeysockets/baileys');
+const { downloadMediaMessage } = require('../../utils/baileys').sync();
 const { webp2png } = require('../../utils/webp2mp4');
 const sharp = require('sharp');
 
